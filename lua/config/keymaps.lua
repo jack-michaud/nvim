21355a2f7b20b4e4ed4a6cac6f84b4cd7f8155eb
Jack Michaud <jack@faynutrition.com>
2023-08-31 12:01:38 -0400

    feat: add some binds and copilot

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,29 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Always include a "desc" in the lua map with a short description of the keymap
+
+-- Hover
+vim.keymap.set(
+  "n",
+  "<leader>h",
+  "<cmd>lua vim.lsp.buf.hover()<CR>",
+  { noremap = true, silent = true, desc = "LSP Hover" }
+)
+
+-- Go to definition
+vim.keymap.set(
+  "n",
+  "<leader>.",
+  "<cmd>lua vim.lsp.buf.definition()<CR>",
+  { noremap = true, silent = true, desc = "LSP Go to definition" }
+)
+
+-- Find references
+vim.keymap.set(
+  "n",
+  "<leader>,",
+  "<cmd>lua vim.lsp.buf.references()<CR>",
+  { noremap = true, silent = true, desc = "LSP Find references" }
+)
