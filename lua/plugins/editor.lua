2ece4ed1c9238ee2fe9420dc8009a0980686af7b
Jack Michaud <jack@lomz.me>
2023-09-05 10:07:13 -0400

    feat: unbind <leader>gs and add formatting via null-ls

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,3 +1,4 @@
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 return {
   -- change some telescope options and a keymap to browse plugin files
   {
@@ -20,12 +21,31 @@ return {
         "<cmd>Telescope live_grep<cr>",
         desc = "Grep",
       },
+      -- Unbinding
       { "<leader>,", false },
+      { "<leader>gs", false },
     },
   },
-  -- Change the default folke/which-key.nvim keymap
-  -- to exclude the <leader>b prefix
   {
     "tpope/vim-fugitive",
+  },
+
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.sources, {
+        require("null-ls").builtins.formatting.black,
+        require("null-ls").builtins.formatting.isort,
+        require("null-ls").builtins.formatting.prettier,
+        require("null-ls").builtins.formatting.dart_format.with({
+          extra_args = { "--line-length=120" },
+        }),
+        require("null-ls").builtins.formatting.nixfmt,
+        require("null-ls").builtins.formatting.gofmt,
+        require("null-ls").builtins.formatting.phpcbf,
+        require("null-ls").builtins.formatting.rustfmt,
+        require("null-ls").builtins.formatting.terraform_fmt,
+      })
+    end,
   },
 }
