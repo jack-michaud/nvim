e89af161b765f3da9a4db719a0deb1d68b21efbf
Jack Michaud <jack@faynutrition.com>
2024-01-05 21:47:28 -0500

    Disable some plugins

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,16 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+return {
+  {
+    "L3MON4D3/LuaSnip",
+    enable = false,
+  },
+  {
+    "akinsho/bufferline.nvim",
+    enable = false,
+  },
+  --
+  -- Pairs brackets, "autopairs"
+  { "echasnovski/mini.nvim", enable = false },
+}
