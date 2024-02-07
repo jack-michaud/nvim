2e166bcfaa4c0ceb84bcaba507a9aefc3e783685
Jack Michaud <jack@faynutrition.com>
2024-02-07 16:09:54 -0500

    Stablize formatter and linter

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -79,90 +79,17 @@ return {
   {
     "nvimtools/none-ls.nvim",
     enabled = false,
-    -- opts = function(_, opts)
-    --   local nls = require("null-ls")
-    --   opts.root_dir = opts.root_dir
-    --     or require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git")
-
-    --   vim.list_extend(opts.sources, {
-    --     nls.builtins.formatting.black,
-    --     nls.builtins.formatting.isort,
-    --     nls.builtins.formatting.prettier,
-    --     nls.builtins.formatting.dart_format.with({
-    --       extra_args = { "--line-length=120" },
-    --     }),
-    --     nls.builtins.formatting.nixfmt,
-    --     nls.builtins.formatting.gofmt,
-    --     nls.builtins.formatting.phpcbf,
-    --     nls.builtins.formatting.rustfmt,
-    --     nls.builtins.formatting.terraform_fmt,
-    --   })
-    -- end,
   },
   {
     "stevearc/conform.nvim",
     -- https://github.com/stevearc/conform.nvim
     enabled = true,
-    opts = function(_, opts)
-      local formatters = require("conform.formatters")
-      formatters.black.command = prefer_bin_from_venv("black")
-      formatters.isort.command = prefer_bin_from_venv("isort")
-      formatters.stylua.args =
-        vim.list_extend({ "--indent-type", "Spaces", "--indent-width", "2" }, formatters.stylua.args)
-
-      local formatters_by_ft = {
-        -- this extends lazyvim's conform setup
-        -- https://www.lazyvim.org/extras/formatting/conform
-        lua = { "stylua" },
-        sh = { "shfmt" },
-        --go = { "gofumpt", "gci" }, -- goimports is not needed as gci is used
-        --protobuf = { "buf" },
-        python = { "isort", "black" },
-        rust = { "rustfmt" },
-        typescript = { "prettier" },
-      }
-
-      -- extend opts.formatters_by_ft
-      -- NOTE: conform.nvim can use a sub-list to run only the first available formatter (see docs)
-      for ft, formatters_ in pairs(formatters_by_ft) do
-        opts.formatters_by_ft[ft] = opts.formatters_by_ft[ft] or {}
-        vim.list_extend(opts.formatters_by_ft[ft], formatters_)
-      end
-    end,
   },
 
   {
     "mfussenegger/nvim-lint",
     -- https://github.com/mfussenegger/nvim-lint
     enabled = true,
-    opts = function(_, opts)
-      local linters = require("lint").linters
-      linters.mypy.cmd = prefer_bin_from_venv("mypy")
-      linters.pylint.cmd = prefer_bin_from_venv("pylint")
-      linters.sqlfluff.args = vim.list_extend({ "--dialect", "postgres" }, linters.sqlfluff.args)
-
-      local linters_by_ft = {
-        --   -- this extends lazyvim's nvim-lint setup
-        --   -- https://www.lazyvim.org/extras/linting/nvim-lint
-        --   protobuf = { "buf", "protolint" },
-        python = { "mypy" },
-        --   sh = { "shellcheck" },
-        --   sql = { "sqlfluff" },
-        --   yaml = { "yamllint" },
-      }
-
-      -- Project specific overrides!
-      -- add pylint if the project is a MindfulData project
-      if vim.fn.getcwd():find("github.com/mindfuldataai") ~= nil then
-        linters_by_ft.python = { "pylint", "mypy" }
-      end
-
-      -- extend opts.linters_by_ft
-      for ft, linters_ in pairs(linters_by_ft) do
-        opts.linters_by_ft[ft] = opts.linters_by_ft[ft] or {}
-        vim.list_extend(opts.linters_by_ft[ft], linters_)
-      end
-    end,
   },
 
   {
@@ -172,21 +99,21 @@ return {
     },
   },
   {
-    {
-      "L3MON4D3/LuaSnip",
-      enable = false,
-    },
-    {
-      "akinsho/bufferline.nvim",
-      enable = false,
-    },
-    -- Pairs brackets, "autopairs"
-    { "echasnovski/mini.pairs", enable = false },
-    {
-      "LazyVim/LazyVim",
-      opts = {
-        colorscheme = "catppuccin",
-      },
+    "L3MON4D3/LuaSnip",
+    keys = function()
+      return {}
+    end,
+  },
+  {
+    "akinsho/bufferline.nvim",
+    enabled = false,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
     },
   },
+  -- Pairs brackets, "autopairs"
+  { "echasnovski/mini.pairs", enabled = false },
 }
