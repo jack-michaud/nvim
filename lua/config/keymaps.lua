53f8a31e6bdf40cba1ef381bc5a773ba164f2b28
Jack Michaud <jack@faynutrition.com>
2024-06-30 09:43:46 -0400

    update and utilities

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,6 +4,42 @@
 
 -- Always include a "desc" in the lua map with a short description of the keymap
 
+function escape_to_json()
+  local selected_text = vim.fn.getreg('"')
+  local escaped_text = vim.fn.json_encode(selected_text)
+  print(escaped_text)
+end
+
+-- Register as command `EscapeToJSON`
+vim.cmd("command! EscapeToJSON :lua escape_to_json()")
+
+function CreateTestFile()
+  local current_file = vim.fn.expand("%:p")
+  local test_file = string.gsub(current_file, "fay/", "tests/")
+  -- Add `test_` prefix to the file base name.
+  test_file = string.gsub(test_file, "([^/]+)$", "test_%1")
+  vim.cmd("vsp " .. test_file)
+end
+
+vim.cmd("command! CreateTestFile :lua CreateTestFile()")
+
+local function get_bufnr()
+  return vim.api.nvim_get_current_buf()
+end
+
+function Get_filepath()
+  local bufnr = get_bufnr()
+  local full_path = vim.api.nvim_buf_get_name(bufnr)
+  -- Get relative path
+  local cwd = vim.fn.getcwd()
+  local rel_path = vim.fn.fnamemodify(full_path, ":~:.")
+
+  if string.find(rel_path, cwd, 1, true) == 1 then
+    return string.sub(rel_path, #cwd + 2)
+  end
+
+  return rel_path
+end
 -- Hover
 vim.keymap.set(
   "n",
