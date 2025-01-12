4ebf982e9496a61aab19db97e25ab5069580c7dd
jack-michaud <mrmichaud42@gmail.com>
2025-01-11 22:13:38 -0500

    Use fzf-lua

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -22,7 +22,7 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
     --{ import = "lazyvim.plugins.extras.lang.typescript" },
-    --{ import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.json" },
--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -57,74 +57,84 @@ end
 
 return {
   -- change some telescope options and a keymap to browse plugin files
+  --{
+  --  "nvim-telescope/telescope.nvim",
+  --  dependencies = { "fcying/telescope-ctags-outline.nvim" },
+  --  keys = {
+  --    -- add a keymap to browse plugin files
+  --    -- stylua: ignore
+  --    {
+  --      "<leader>B",
+  --      "<cmd>Telescope buffers<cr>",
+  --      desc = "Buffers",
+  --    },
+  --    {
+  --      "<leader>e",
+  --      "<cmd>Telescope file_browser<cr>",
+  --      desc = "File browser",
+  --    },
+  --    {
+  --      "<leader>F",
+  --      "<cmd>Telescope find_files<cr>",
+  --      desc = "Find Files",
+  --    },
+  --    {
+  --      "<leader>G",
+  --      "<cmd>Telescope live_grep<cr>",
+  --      desc = "Grep",
+  --    },
+  --    -- Unbinding
+  --    { "<leader>,", false },
+  --    { "<leader>t", "<cmd>Telescope ctags_outline<cr>", desc = "Ctags Outline" },
+  --  },
+  --  opts = function()
+  --    -- require ctags_outline extension
+  --    require("telescope").load_extension("ctags_outline")
+  --    require("telescope").load_extension("file_browser")
+
+  --    local fb_actions = require("telescope").extensions.file_browser.actions
+
+  --    return {
+  --      extensions = {
+  --        ctags_outline = {
+  --          --ctags option
+  --          ctags = { "ctags" },
+  --          --ctags filetype option
+  --          ft_opt = {
+  --            -- vim = "--vim-kinds=fk",
+  --            -- javascript = "--javascript-kinds=f",
+  --            -- lua = "--lua-kinds=fk",
+  --            -- python = "--python-kinds=fm --language-force=Python",
+  --            -- typescript = "--typescript-kinds=f",
+  --          },
+  --        },
+  --        file_browser = {
+  --          theme = "dropdown",
+  --          mappings = {
+  --            ["i"] = {
+  --              ["<C-t>"] = fb_actions.change_cwd,
+  --              ["<C-r>"] = fb_actions.create,
+  --            },
+  --            ["n"] = {},
+  --          },
+  --        },
+  --      },
+  --    }
+  --  end,
+  --},
+  --{
+  --  "nvim-telescope/telescope-file-browser.nvim",
+  --  dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+  --},
   {
-    "nvim-telescope/telescope.nvim",
-    dependencies = { "fcying/telescope-ctags-outline.nvim" },
+    "ibhagwan/fzf-lua",
     keys = {
-      -- add a keymap to browse plugin files
-      -- stylua: ignore
-      {
-        "<leader>B",
-        "<cmd>Telescope buffers<cr>",
-        desc = "Buffers",
-      },
-      {
-        "<leader>e",
-        "<cmd>Telescope file_browser<cr>",
-        desc = "File browser",
-      },
-      {
-        "<leader>F",
-        "<cmd>Telescope find_files<cr>",
-        desc = "Find Files",
-      },
-      {
-        "<leader>G",
-        "<cmd>Telescope live_grep<cr>",
-        desc = "Grep",
-      },
+      { "<leader>B", "<cmd>FzfLua buffers<cr>", desc = "Switch buffers" },
+      { "<leader>F", "<cmd>FzfLua files<cr>", desc = "Find Files" },
+      { "<leader>G", "<cmd>FzfLua live_grep<cr>", desc = "Grep" },
       -- Unbinding
       { "<leader>,", false },
-      { "<leader>t", "<cmd>Telescope ctags_outline<cr>", desc = "Ctags Outline" },
-    },
-    opts = function()
-      -- require ctags_outline extension
-      require("telescope").load_extension("ctags_outline")
-      require("telescope").load_extension("file_browser")
-
-      local fb_actions = require("telescope").extensions.file_browser.actions
-
-      return {
-        extensions = {
-          ctags_outline = {
-            --ctags option
-            ctags = { "ctags" },
-            --ctags filetype option
-            ft_opt = {
-              -- vim = "--vim-kinds=fk",
-              -- javascript = "--javascript-kinds=f",
-              -- lua = "--lua-kinds=fk",
-              -- python = "--python-kinds=fm --language-force=Python",
-              -- typescript = "--typescript-kinds=f",
-            },
-          },
-          file_browser = {
-            theme = "dropdown",
-            mappings = {
-              ["i"] = {
-                ["<C-t>"] = fb_actions.change_cwd,
-                ["<C-r>"] = fb_actions.create,
-              },
-              ["n"] = {},
-            },
-          },
-        },
-      }
-    end,
-  },
-  {
-    "nvim-telescope/telescope-file-browser.nvim",
-    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+    }
   },
   {
     "tpope/vim-fugitive",
@@ -229,7 +239,7 @@ return {
       local mypy_config = require("lint").linters.mypy
 
       mypy_config.append_fname = false
-      mypy_config.cmd = prefer_bin_from_venv("dmypy")
+      mypy_config.cmd = "dmypy"
       mypy_config.args = {
           'run',
           '--',
