d9ed1aabbdc188f427059a36a63bc9fba2c10e38
Jack Michaud <jack@lomz.me>
2025-11-16 12:24:26 -0500

    Move lsp and formatting

---
+++ lua/plugins/lsp/lspconfig.lua
@@ -0,0 +1,28 @@
+utils = require("common.utils")
+
+return {
+  {
+    -- lsp config
+    "nvim-lspconfig",
+    opts = function(opts)
+      local pythonPath = utils.prefer_bin_from_venv("python")
+      local ret = {
+        setup = {
+          -- pyright = {
+          --   enabled = true,
+          --   settings = {
+          --     python = {
+          --       pythonPath = pythonPath,
+          --       analysis = { diagnosticMode = "off", typeCheckingMode = "off" },
+          --     },
+          --   },
+          -- },
+          ruff_lsp = {
+            enabled = true,
+          },
+        },
+      }
+      return ret
+    end,
+  },
+}
